4767b908fdfe05cc89a06aa089bf4eae5417ab1f
Pervaiz Musharaf <pervaiz.dev@pk.see.biz>
2023-09-08 15:46:28 +0500

    fix() configure docker on local project

--- server.js
+++ server.js
@@ -1,22 +1,32 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-
+const mongoose = require("mongoose");
+const config = require("./config/config")
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
+ mongoose.Promise = global.Promise;
+  const db = mongoose.connect(config.ssoDB, {});
+  console.log("Successfully Connected To MongoDB.");
+  mongoose.connection.on("error", (err) => {
+    console.log(
+      "Error: Could not connect to MongoDB. Did you forget to run `mongod`?".red
+    );
+  });
 io.on('connection', (socket) => {
   console.log('A user connected');
 
   socket.on('message', (message) => {
     console.log('Received message:', message);
-    io.emit('message', message);
+    io.emit('message', "socket work proper");
   });
 
   socket.on('disconnect', () => {
     console.log('A user disconnected');
   });
 });
+
 
 // app.set("socketio", io);
 // io.on("connection", function (socket) {
@@ -25,8 +35,15 @@ io.on('connection', (socket) => {
 //     console.log("disconnected!");
 //   });
 // });
+app.get("/",async(req,res)=>{
+  res.send(`Welcome to SSO Backend API.`)
+})
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+const PORT = config.ssoPort || 3001;
+// server.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+server.listen(PORT, '0.0.0.0',() => {
+  console.info(`sso-server backend  listening on port ${PORT}`);
 });
